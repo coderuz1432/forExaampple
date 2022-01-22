0774673e28099e53fb75669c3eac288abcf0086b
Jamshid <jamshidbek1432@gmail.com>
2022-01-22 15:47:11 +0500

    birnarsasi bor

---
+++ main.js
@@ -0,0 +1,14 @@
+//let username = prompt("Whats your name ?")
+//let sur_name = prompt('Whats your surname')
+//let ag_e = prompt('How old are you ?')
+//let jo_b = prompt('Whats your job ?')
+//let user = {
+//    name: username,
+//    surname: sur_name,
+//    age: ag_e,
+//    job: jo_b
+//}
+//console.log(user)
+//console.log(sur_name)
+//console.log(ag_e);
+//console.log(jo_b);
